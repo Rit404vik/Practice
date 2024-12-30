cd8b1a52b8293caee65591ac2cfa8b623854969d
Rit404vik <ritviknarang993@gmail.com>
2024-12-30 19:34:59 +0530

    Added geometric operations for a circle in the calc

--- Calculator.java
+++ Calculator.java
@@ -16,8 +16,13 @@ public class Calculator {
         int inputTwo = 0;
         BigInteger count = new BigInteger("1");
         int nums;
+        final double pi = Math.PI;
+        double radius;
+
         String choice;
         String operator;
+        String circleOfChoice;
+
         boolean keep = true;
 
         while (keep) {
@@ -25,7 +30,8 @@ public class Calculator {
             System.out.println("\n1. Enter two numbers");
             System.out.println("2. Perform an arithmetic operation");
             System.out.println("3. Factorial of a number");
-            System.out.println("4. Exit");
+            System.out.println("4. Perform Geometric Calculations on a Circle");
+            System.out.println("5. Exit");
             System.out.print("Enter your option : ");
             choice = reader.readLine();
 
@@ -62,13 +68,32 @@ public class Calculator {
                         System.out.println(count);
                         count = new BigInteger("1");
                     }
-                    case "4" -> keep = false;
+                    case "4" -> {
+                        System.out.print("Enter the radius => ");
+                        radius = input.nextDouble();
+
+                        System.out.println("Select which formula of circle you want to apply : ");
+                        System.out.println("1. Circumference of a circle");
+                        System.out.println("2. Area of a circle");
+                        System.out.println("3. Volume of a sphere");
+                        System.out.println("4. Surface Area of a sphere");
+                        System.out.print("Enter : ");
+
+                        circleOfChoice = reader.readLine();
+
+                        switch (circleOfChoice){
+                            case "1" -> System.out.println("Circumference : " + 2.0 * pi * radius);
+                            case "2" -> System.out.println("Area : " + pi * (radius * radius));
+                            case "3" -> System.out.println("Volume : " + (4.0/3.0) * (pi) *(radius * radius * radius));
+                            case "4" -> System.out.println("Surface Area : " + 4.0 * pi * (radius * radius));
+                        }
+                    }
+                    case "5" -> keep = false;
                 }
             } catch (ArithmeticException e){
                 System.out.println(e.getMessage());
                 System.out.println("Re-Enter the numbers");
             }
-
         }
     }
 }
