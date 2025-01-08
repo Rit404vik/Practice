eb608305d667bcaaecd8d0e0b12f5a9a27e70263
Rit404vik <ritviknarang993@gmail.com>
2025-01-08 21:52:15 +0530

    A simple code to test the time difference between FileWriter without buffering and with buffering.

---
+++ FileSpeedComparison.java
@@ -0,0 +1,55 @@
+import java.io.*;
+
+public class FileSpeedComparison {
+    public static void writeWithoutBuffer(String filepath){
+        try (FileWriter writer = new FileWriter(filepath)){
+            for(int i = 0; i<1000; i++){
+                writer.write("Line" + (i+1) + "Writing without Buffer\n");
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeWithBuffer(String filepath){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filepath))) {
+            for (int i = 0; i<1000; i++){
+                writer.write("Line" + (i+1) + "Writing with Buffer");
+                writer.newLine();
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void compareWriteMethods(String filepath){
+
+        long startTimeWithoutBuffer = System.nanoTime();
+        writeWithoutBuffer(filepath);
+        long endTimeWithoutBuffer = System.nanoTime();
+        long durationBetweenWithoutBuffer = endTimeWithoutBuffer - startTimeWithoutBuffer;
+        System.out.println("Time taken without buffering: " + durationBetweenWithoutBuffer + " nanoseconds");
+
+        long startTimeWithBuffer = System.nanoTime();
+        writeWithBuffer(filepath);
+        long endTimeWithBuffer = System.nanoTime();
+        long durationBetweenWithBuffer = endTimeWithBuffer - startTimeWithBuffer;
+        System.out.println("Time taken with buffering: " + durationBetweenWithBuffer + " nanoseconds");
+
+        System.out.println("---------------------");
+
+        if (durationBetweenWithBuffer < durationBetweenWithoutBuffer){
+            System.out.println("Time with buffer is faster " + durationBetweenWithBuffer);
+        } else {
+            System.out.println("Time without buffer is faster " + durationBetweenWithoutBuffer);
+        }
+
+    }
+    public static void main(String[] args) {
+
+        String filepath = "output.txt";
+
+        compareWriteMethods(filepath);
+
+    }
+}
